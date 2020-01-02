b7afdfadf651bee203bc176cca6c94059fd35d49
hxe <1376246506@qq.com>
2020-01-02 19:26:45 +0800

    modify rem.js & reset.css

--- vv/src/assets/rem.js
+++ vv/src/assets/rem.js
@@ -1,4 +1,4 @@
-; (function(win, lib) {
+;(function (win, lib) {
     var doc = win.document;
     var docEl = doc.documentElement;
     var metaEl = doc.querySelector('meta[name="viewport"]');
@@ -54,7 +54,7 @@
     if (!metaEl) {
         metaEl = doc.createElement("meta");
         metaEl.setAttribute("name", "viewport");
-        if ( !! win.webPageScalable) {
+        if (!!win.webPageScalable) {
             metaEl.setAttribute("content", "initial-scale=" + scale + ", user-scalable=yes")
         } else {
             metaEl.setAttribute("content", "initial-scale=" + scale + ", maximum-scale=" + scale + ", minimum-scale=" + scale + ", user-scalable=no viewport-fit=cover")
@@ -79,40 +79,37 @@
         docEl.style.fontSize = rem + "px";
         flexible.rem = win.rem = rem
     }
-    win.addEventListener("resize",
-    function() {
-        clearTimeout(tid);
-        tid = setTimeout(refreshRem, 300)
-    },
-    false);
-    win.addEventListener("pageshow",
-    function(e) {
-        if (e.persisted) {
+    win.addEventListener("resize", function () {
             clearTimeout(tid);
             tid = setTimeout(refreshRem, 300)
-        }
-    },
-    false);
+        },
+        false);
+    win.addEventListener("pageshow", function (e) {
+            if (e.persisted) {
+                clearTimeout(tid);
+                tid = setTimeout(refreshRem, 300)
+            }
+        },
+        false);
     if (doc.readyState === "complete") {
         doc.body.style.fontSize = 12 * dpr + "px"
     } else {
-        doc.addEventListener("DOMContentLoaded",
-        function(e) {
-            doc.body.style.fontSize = 12 * dpr + "px"
-        },
-        false)
+        doc.addEventListener("DOMContentLoaded", function (e) {
+                doc.body.style.fontSize = 12 * dpr + "px"
+            },
+            false)
     }
     refreshRem();
     flexible.dpr = win.dpr = dpr;
     flexible.refreshRem = refreshRem;
-    flexible.rem2px = function(d) {
+    flexible.rem2px = function (d) {
         var val = parseFloat(d) * this.rem;
         if (typeof d === "string" && d.match(/rem$/)) {
             val += "px"
         }
         return val
     };
-    flexible.px2rem = function(d) {
+    flexible.px2rem = function (d) {
         var val = parseFloat(d) / this.rem;
         if (typeof d === "string" && d.match(/px$/)) {
             val += "rem"
